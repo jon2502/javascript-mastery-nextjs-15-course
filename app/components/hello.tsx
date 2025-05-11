9e875d33aca9c9905297a7e8504c1520d59c1d27
jon2502 <jonbjarke25@outlook.dk>
2025-05-11 11:18:49 +0200

    setup start of project learned about server and client side in nextjs

---
+++ app/components/hello.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+function Hello(){
+    console.log("i am a client component")
+
+    return(
+        <div>
+            <h1>Hello</h1>
+        </div>
+    )
+
+}
+
+export default Hello;
