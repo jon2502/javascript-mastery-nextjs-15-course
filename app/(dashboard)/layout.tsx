281eaf886fbc14537de67e6a91c14bcb14fe4944
jon2502 <jonbjarke25@outlook.dk>
2025-05-11 11:55:17 +0200

    added route groupes

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Layout = ({children}: {children: React.ReactNode}) => {
   return (
     <div>
-        <h1 className='text-3xl'>Layout</h1>
+        <h1 className='text-3xl'>Layout dashboard</h1>
         {children}
     </div>
   )
