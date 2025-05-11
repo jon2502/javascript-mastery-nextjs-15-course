7393a74c9bf3ae115258f42e1181e4bb6d02676b
jon2502 <jonbjarke25@outlook.dk>
2025-05-11 11:39:34 +0200

    added parameter to user profile page

--- app/dashboard/users/[id]/page.tsx
+++ app/dashboard/users/[id]/page.tsx
@@ -1,9 +1,14 @@
 import React from 'react'
 
-const page = () => {
-  return (
-    <div>User details</div>
-  )
+const page = ({params}: {params: {id: string}}) => {
+    const {id} = params;
+
+    return (
+        <div>
+            <h1>User details page</h1>
+            <h2>User profile {id}</h2>
+        </div>
+    )
 }
 
 export default page
