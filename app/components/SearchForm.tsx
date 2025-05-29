2d2018d561543ef8ec772db5a529655f5ff90665
jon2502 <jonbjarke25@outlook.dk>
2025-05-29 07:09:37 +0200

    setup header

---
+++ app/components/SearchForm.tsx
@@ -0,0 +1,27 @@
+import Form from 'next/form'
+import SearchFormReset from './SearchFormReset'
+
+const SearchForm = () => {
+    const query  = "test"
+
+
+
+    return (
+    <Form action="/search" scroll={false} className='search-form'>
+        <input
+            name="query"
+            defaultValue={query}
+            className="search-input"
+            placeholder="Search Startups"
+        />
+        <div className="flex gap-2">
+            {query && <SearchFormReset />}
+            <button type="submit" className="search-btn text-white">
+                S
+            </button>
+        </div>
+    </Form>
+    )
+}
+
+export default SearchForm
