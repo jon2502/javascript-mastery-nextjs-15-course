1ba52def3a953064480051527892facaafffe0f1
jon2502 <jonbjarke25@outlook.dk>
2025-06-25 08:54:37 +0200

    setup of startupcard template

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,24 @@
 import SearchForm from "@/components/SearchForm"
+import StartupCard from "@/components/StartupCard";
 
 export default async function Home( { searchParams }: {
   searchParams: Promise<{ query?: string }>;
 }) {
   const query = (await searchParams).query;
+
+  const posts = [{
+    _createdAT: new Date(),
+    views: 55,
+    author: {
+      _id: 1,
+      name: "Greg"
+    },
+    _id: 1,
+    description: "this is a description",
+    image: "https://cards.deckstats.net/picture.php?utf8=1&lng=en&card=Y%27shtola%2C+Night%27s+Blessed&set=FIC&collector_number=215&language_code=en",
+    category: "Cards",
+    title: "MTG cards"
+  }]
 
   return (
     <>
@@ -13,6 +28,19 @@ export default async function Home( { searchParams }: {
           Submit Ideas, Vote on Pitches, and Get Noticed in Virtual Competitions.
         </p>
         <SearchForm query ={query}/>
+      </section>
+      <section className="section_container">
+        <p className="text-30-semibold">
+          {query ? `Search Results for ${query}`: "All Startups"}
+        </p>
+        <ul className="mt-7 card_grid">
+          {posts?.length > 0 ? (
+            posts.map((post: StartupTypeCard) =>(
+              <StartupCard key={post?._id} post={post} />
+            ))) : (
+              <p className="no-results">No startups found</p>
+          )}
+        </ul>
       </section>
     </>
     
