cdcf6387464d4da55fd48f89280191804e2d6aee
jon2502 <jonbjarke25@outlook.dk>
2025-06-25 12:25:14 +0200

    setup pages for startup

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -10,7 +10,9 @@ export default async function Home( { searchParams }: {
 }) {
   const query = (await searchParams).query;
 
-  const {data: posts} = await sanityFetch({query:STARTUP_QUERY});
+  const params = {search: query || null}
+
+  const {data: posts} = await sanityFetch({query:STARTUP_QUERY, params});
 
   return (
     <>
--- lib/queries.ts
+++ lib/queries.ts
@@ -1,7 +1,7 @@
 import { defineQuery } from "next-sanity";
 
 export const STARTUP_QUERY = defineQuery(
-    `*[_type == "startup" && defined(slug.current)] | order(_createdAT desc){
+    `*[_type == "startup" && defined(slug.current) && !defined($search) || title match $search || category match $search || author->name match $search] | order(_createdAT desc){
   _id,
   title,
   slug,
