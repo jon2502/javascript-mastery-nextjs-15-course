c53a70b0633c9ae0b1078a7ec8405577e53cdb83
jon2502 <jonbjarke25@outlook.dk>
2025-08-02 15:12:21 +0200

    updated navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 import Link from 'next/link'
 import Image from 'next/image'
 import { auth, signOut, signIn } from '@/auth'
+import { BadgePlus, LogOut } from 'lucide-react'
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
 
 const Navbar = async () => {
@@ -17,17 +19,27 @@ const Navbar = async () => {
                 {session && session?.user ? (
                     <>
                         <Link href="/startup/create">
-                            <span>Create</span>
+                            <span className='max-sm:hidden'>Create</span>
+                            <BadgePlus className="size-6 sm:hidden text-red-500" />
                         </Link>
                         <form action={async ()=>{
                             "use server";
                             await signOut({redirectTo:"/"})
                         }}>
-                            <button type='submit'>logout</button>
+                            <button type='submit'>
+                                <span className='max-sm:hidden'>Logout</span>
+                                <LogOut className="size-6 sm:hidden text-red-500" />
+                            </button>
                         </form>
 
                         <Link href={`/user/${session?.id}`}>
-                            <span>{session?.user?.name}</span>
+                            <Avatar className="size-10">
+                                <AvatarImage
+                                    src={session?.user?.image || ""}
+                                    alt={session?.user?.name || ""}
+                                />
+                                <AvatarFallback>AV</AvatarFallback>
+                            </Avatar>
                         </Link>
                     </>
                 ) : (
